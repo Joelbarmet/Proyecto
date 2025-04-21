ee9ce2f9797ea0ecf3fbd5acfda02301f050babe
Joel Barreira <joel.barreira.iesdoctorbalmis.com>
2025-04-21 11:35:40 +0200

    commit

--- includes/clientes.php
+++
@@ -1,9 +0,0 @@
-<?php
-$conexion = new mysqli("localhost", "root", "", "plumbing");
-
-if ($conexion->connect_error) {
-    die("Error de conexión: " . $conexion->connect_error);
-}
-$sql = "SELECT * FROM clientes";
-$resultado = $conexion->query($sql);
-?>
