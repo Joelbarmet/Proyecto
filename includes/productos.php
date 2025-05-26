383ed5c632f824e94cbd03e1519710289ca3cb65
Joel Barreira <joel.barreira.iesdoctorbalmis.com>
2025-05-26 16:31:11 +0200

    Primer commit

---
+++ includes/productos.php
@@ -0,0 +1,39 @@
+<?php
+
+$servidor = "localhost";
+$usuarioDb = "root";
+$contraseñaDb = "";
+$baseDeDatos = "plumbing";
+
+$conexion = new mysqli($servidor, $usuarioDb, $contraseñaDb, $baseDeDatos);
+
+if (!$conexion) {
+    die("Error al conectar con la base de datos: " . mysqli_connect_error());
+}
+
+$consulta = "SELECT * FROM productos";
+
+$resultado = $conexion->query($consulta);
+
+if ($resultado->num_rows > 0) {
+    echo "<table border='1'>";
+    echo "<tr><th>ID</th><th>Nombre</th><th>Descripcion</th><th>Precio</th><th>Stock</th></tr>";
+
+    while ($fila = $resultado->fetch_assoc()) {
+        echo "<tr>";
+        echo "<td>" . $fila['id'] . "</td>";
+        echo "<td>" . $fila['nombre'] . "</td>";
+        echo "<td>" . $fila['descripcion'] . "</td>";
+        echo "<td>" . $fila['precio'] . "</td>";
+        echo "<td>" . $fila['stock'] . "</td>";
+        echo "</tr>";
+    }
+
+    echo "</table>";
+} else {
+    echo "No hay resultados en la tabla.";
+}
+
+$conexion->close();
+?>
+<link rel="stylesheet" href="../assets/css/styles.css" />
